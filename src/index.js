1cb47cf131591961bba07fabf1df57b8433bc613
lucas.miguel <lucas.miguel@b2wdigital.com>
2017-07-06 18:14:29 -0300

    preparando para fazer chamadas para phone só quando for necessário

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ const phonesRepo = new Repository();
 salesmenRepo.save({name: 'Lucas', phones: ['1', '2', '3']});
 salesmenRepo.save({name: 'Valdir', phones: ['2']});
 salesmenRepo.save({name: 'Chico', phones: []});
-salesmenRepo.save({name: 'Gabriel'});
 
 phonesRepo.save({name: 'Moto G1'});
 phonesRepo.save({name: 'Moto G2'});
@@ -19,20 +18,23 @@ phonesRepo.save({name: 'Moto G3'});
 
 const completeSalesman = (salesman) => {
   let phones = [];
-  
-  if (R.type(salesman.phones) === 'Array') {
-    salesman.phones = salesman.phones.map(phoneId => phonesRepo.find(phoneId));
-  } else {
-    salesman.phones = [];
-  }
-  
+
+  salesman.phones = salesman.phones.map(phoneId => phonesRepo.find(phoneId));
+
   return salesman;
 };
 
 const root = {
   addSalesman: ({salesman}) => salesmenRepo.save(salesman),
   salesman: ({id}) => salesmenRepo.find(id).then(completeSalesman),
-  salesmen: () => salesmenRepo.all().then(salesmen => salesmen.map(completeSalesman)),
+  salesmen: (_, _, {fieldNodes}) => {
+    // console.log('obj', obj);
+    // console.log('args', args);
+    // fieldNodes.selectionSet.selections[x].name.value === 'phones'
+    // console.log('context', JSON.stringify(fieldNodes));
+
+    return salesmenRepo.all().then(salesmen => salesmen.map(completeSalesman));
+  },
 
   addPhone: ({phone}) => phonesRepo.save(salesman),
   phone: ({id}) => phonesRepo.find(id),
